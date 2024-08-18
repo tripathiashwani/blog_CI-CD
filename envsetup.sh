c373525bd7f65461af96326881d6cda5d48e68c3
ASHWANI TRIPATHI <tashwani475@gmail.com>
2024-08-18 17:12:17 +0530

    jenkins intial setup

---
+++ envsetup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ -d "env" ] 
+then
+    echo "Python virtual environment exists." 
+else
+    python -m venv env
+fi
+
+echo $PWD
+source env/scripts/activate
+
+
+pip3 install -r requirements.txt
+
+if [ -d "logs" ] 
+then
+    echo "Log folder exists." 
+else
+    mkdir logs
+    touch logs/error.log logs/access.log
+fi
+
+sudo chmod -R 777 logs
